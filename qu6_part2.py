9f7d5eb20327c7ca905235a433abdf19cf5f3bff
moni310 <72276446+moni310@users.noreply.github.com>
2020-11-16 16:33:00 +0530

    Add files via upload

---
+++ qu6_part2.py
@@ -0,0 +1,36 @@
+def calculater(n1,n2,operation):
+    if operation =="+":
+        num1=n1+n2
+        return (num1)
+    elif operation =="-":
+        num2=n1-n2
+        return(num2)
+    elif operation =="*":
+        num3=n1*n2
+        return(num3)
+    elif operation =="%":
+        num4=n1%n2
+        return(num4)
+    elif operation =="//":
+        num5=n1//n2
+        return(num5)
+operation=input("enter the operation")
+number=int(input("enter the number"))
+number1=int(input("enter the number"))
+print(calculater(number,number1,operation))
+
+operation=input("enter the operation")
+number=int(input("enter the number"))
+number1=int(input("enter the number"))
+print(calculater(number,number1,operation))
+
+operation=input("enter the operation")
+number=int(input("enter the number"))
+number1=int(input("enter the number"))
+print(calculater(number,number1,operation))
+
+operation=input("enter the operation")
+number=int(input("enter the number"))
+number1=int(input("enter the number"))
+print(calculater(number,number1,operation))
+
