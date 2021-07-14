4e5fe4e68be96d65041f2b299c1623d1d3598832
moni310 <72276446+moni310@users.noreply.github.com>
2021-07-14 11:09:41 +0530

    Add files via upload

---
+++ sec_max.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number"))
+num1=int(input("enter the number"))
+num2=int(input("enter the  number"))
+if num >num1  and num>num2:
+    print(num,"bada h")
+elif num1 >num and num1 >num2:
+    print(num1,"bada h")
+else:
+    print(num2,"bada h")
